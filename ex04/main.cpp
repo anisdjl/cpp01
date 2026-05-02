70841670686d90bacaa23cdfbeefe7e9190130d9
anisdjl <adjelili954@gmail.com>
2026-05-02 15:27:13 +0200

    cpp

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anis <anis@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 21:53:22 by anis              #+#    #+#             */
-/*   Updated: 2026/05/01 23:12:36 by anis             ###   ########.fr       */
+/*   Updated: 2026/05/02 15:25:58 by adjelili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,43 @@ int main(int argc, char *argv[])
 {
 	if (argc != 4)
 	{
-		std::cout << "Try with this inputs: <filename> <s1> <s2>\n";
+		std::cerr << "Try with this inputs: <filename> <s1> <s2>\n";
 		return (1);
 	}
 	std::string	filename = argv[1];
 	std::string s1 = argv[2];
 	std::string s2 = argv[3];
-
+	if (s1.empty())
+	{
+		std::cout << "<s1> is empty, try with a non empty string\n";
+		return (1);
+	}
+	std::string	outfile_name = filename + ".replace";
+	std::string line;
 	std::ifstream	infile(filename);
 	if (!infile.is_open())
 	{
-		std::cout << "Error while opening the file: bad permission or no such file\n";
+		std::cerr << "Error while opening the file: bad permission or no such file\n";
 		return (1);
 	}
-	
-	
+	std::ofstream	outfile(outfile_name);
+	while (getline(infile, line))
+	{
+		algo(line, s1, s2);
+		outfile << line << std::endl;
+	}
+	infile.close();
+	outfile.close();
 	return (0);
+}
+
+void	algo(std::string &line, std::string s1, std::string s2)
+{
+	size_t res = line.find(s1, 0);
+	while (res != std::string::npos)
+	{
+		line.erase(res, s1.length());
+		line.insert(res, s2);
+		res = line.find(s1, res + s2.length());
+	}
 }
--- ex04/main.hpp
+++ ex04/main.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anis <anis@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: adjelili <adjelili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 21:53:42 by anis              #+#    #+#             */
-/*   Updated: 2026/05/01 22:25:34 by anis             ###   ########.fr       */
+/*   Updated: 2026/05/02 14:44:26 by adjelili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+
+void	algo(std::string &line, std::string s1, std::string s2);
 
 #endif
